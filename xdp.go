49180cd29dec39b2782a8b8bec251736e68f6026
Xie Weineng <xieweineng@gmail.com>
2025-04-29 10:07:36 +0800

    fix error compile with new golang.org/x/sys

--- xdp.go
+++ xdp.go
@@ -232,10 +232,10 @@ func NewSocket(Ifindex int, QueueID int, options *SocketOptions) (xsk *Socket, e
 	}
 
 	xdpUmemReg := unix.XDPUmemReg{
-		Addr:       uint64(uintptr(unsafe.Pointer(&xsk.umem[0]))),
-		Len:        uint64(len(xsk.umem)),
-		Chunk_size: uint32(options.FrameSize),
-		Headroom:   0,
+		Addr:     uint64(uintptr(unsafe.Pointer(&xsk.umem[0]))),
+		Len:      uint64(len(xsk.umem)),
+		Size:     uint32(options.FrameSize),
+		Headroom: 0,
 	}
 
 	var errno syscall.Errno
